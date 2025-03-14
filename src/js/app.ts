583ca1bcd9a3114f89f3eec1096ad6eeae5b637d
Cristian Marcelo de Picciotto <cmdepicciotto@gmail.com>
2025-03-14 14:48:27 -0300

    feat: init [#1]

---
+++ src/js/app.ts
@@ -0,0 +1,5 @@
+/**
+ * @description App
+ * @author      C. M. de Picciotto <d3p1@d3p1.dev> (https://d3p1.dev/)
+ */
+export default class App {}
---
+++ vite.config.js
@@ -0,0 +1,12 @@
+export default {
+  root: 'src/',
+  base: '/bad-apple-noise-effect/',
+  server: {
+    host: true,
+  },
+  build: {
+    outDir: '../docs',
+    emptyOutDir: true,
+    sourcemap: true,
+  },
+}
